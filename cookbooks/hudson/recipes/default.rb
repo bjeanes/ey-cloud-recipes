13d5665812c1b338578cb08064455b1932e8805d
Bodaniel Jeanes <me@bjeanes.com>
2010-10-11 11:45:09 -0700

    Move a lot of the init configuration into chef variables for easy changes + DRY

--- cookbooks/hudson/recipes/default.rb
+++ cookbooks/hudson/recipes/default.rb
@@ -9,9 +9,11 @@
 # We'll assume running hudson under the default username
 hudson_user = node[:users].first[:username]
 hudson_port = 8082
+hudson_home = "/opt/hudson"
+hudson_pid  = "#{hudson_home}/tmp/pid"
 
 %w[logs tmp war .].each do |dir|
-  directory "/opt/hudson/#{dir}" do
+  directory "#{hudson_home}/#{dir}" do
     owner hudson_user
     group hudson_user
     mode  0755 unless dir == "war"
@@ -20,7 +22,7 @@ hudson_port = 8082
   end
 end
 
-remote_file "/opt/hudson/hudson.war" do
+remote_file "#{hudson_home}/hudson.war" do
   source "http://hudson-ci.org/latest/hudson.war"
   owner hudson_user
   group hudson_user
@@ -33,10 +35,15 @@ template "/etc/init.d/hudson" do
   owner "root"
   group "root"
   mode 0755
-  variables(:user => hudson_user, :port => hudson_port)
+  variables(
+    :user => hudson_user,
+    :port => hudson_port,
+    :home => hudson_home,
+    :pid  => hudson_pid
+  )
   not_if { FileTest.exists?("/etc/init.d/hudson") }
 end
 
 execute "ensure-hudson-is-running" do
-  command "/etc/init.d/hudson start && ps aux | grep `cat /opt/hudson/cc.pid`"
+  command "/etc/init.d/hudson start && ps aux | grep `cat #{hudson_pid}`"
 end
